ce8fe2004883ca1cfb08cd12ee9572696894014f
Sahas Prajapati <b10prajapati@gmail.com>
2020-11-12 19:44:04 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+from scipy.sparse import coo_matrix
+import numpy as np
+import tensorflow as tf
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(e):
+  return jsonify({"error": "Page not found error"})
+  
+@app.errorhandler(405)
+def method_not_allowed(e):
+  return jsonify({"error": "Method Not Allowed"})
+
+@app.route('/')
+def inform():
+  return jsonify({"error": "Call /predict/<model-name> in post method with body with user prefernce to get prediction"})
+
+@app.route('/predict/<string:model_name>', methods=['POST'])
+def Prediction(model_name):
+  json = request.get_json()
+
+  if model_name=='multvae':
+    vae = multvae
+  else:
+    return jsonify({"error":"Model Not Found"})
+  k = 10
+  
+  preferred_movies = json['preferred_movies']
+  size = len(preferred_movies)
+  data = np.ones((size)).astype('int')
+  row = np.zeros((size)).astype('int')
+  col = np.array(preferred_movies)
+
+  user_matrix = np.array(coo_matrix((data, (row, col)), shape=( 1,62000)).todense())
+  
+  reconstructed_matrix = vae.decoder(vae.encoder(user_matrix)).numpy()
+
+  sorted_ratings = reconstructed_matrix[0].tolist()
+
+  top_predicted_movies_idx = sorted(range(len(sorted_ratings)), key=lambda i: sorted_ratings[i])[-k:]
+
+  print(top_predicted_movies_idx)
+  return jsonify({"error": "", "predictions": top_predicted_movies_idx})
+
+
+if __name__=='__main__':
+  multvae = tf.keras.models.load_model('models/multvae')
+
+  app.run()
---
+++ models/multvae/saved_model.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c705e37e73c7ae700132186fb475402a6bfaae6e7dfcc7f7bae9bbe5394bc0f
+size 231495
